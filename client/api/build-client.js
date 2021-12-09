b503e05bed6769a41114ff947fec88bdaefa34af
Gordon Cheng <gordoncheng@outlook.com>
2021-12-09 13:21:51 +0800

    update ingress service

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ export default ({ req }) => {
 
     return axios.create({
       baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+        'http://ensembletech.xyz',
       headers: req.headers,
     });
   } else {
